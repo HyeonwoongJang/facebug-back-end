6cf9dad1f75a21fc8f700c25ad6065c9dac0bcb6
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-10-20 20:36:31 +0900

    Feat: 이메일 중복 체크 기능 구현 완료

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from user.models import User
 from user.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,6 +20,13 @@ class RegisterView(APIView):
 class EmailCheckView(APIView):
     def post(self, request):
         """이메일 중복 검사를 위한 클래스 뷰입니다."""
+        email = User.objects.filter(email=request.data['email'])
+        print(request.data['email'])
+        print(email)
+        if email : 
+            return Response({'message':'해당 이메일은 이미 사용 중입니다.'}, status=status.HTTP_409_CONFLICT)
+        else:
+            return Response({'message':'해당 이메일은 사용 가능합니다.'}, status=status.HTTP_200_OK)
 
 class NicknameCheckView(APIView):
     def post(self, request):
