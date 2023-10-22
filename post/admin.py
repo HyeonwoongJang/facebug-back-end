776d1880808f298b5be8fc8591762b13bcd723eb
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-10-22 20:33:11 +0900

    Add: 관리자 페이지에 Comment모델 추가

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from post.models import Post, Image
+from post.models import Comment, Post, Image
 
 admin.site.register(Post)
 admin.site.register(Image)
+admin.site.register(Comment)
