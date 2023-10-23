6fb8e341e27b456cbcf217b1384ed96e335c17b3
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-10-23 16:59:03 +0900

    Fix: 회원가입시 프로필 이미지를 등록하지 않을 경우 디폴트 이미지 경로를 설정해주기 위해 조건식으로 변경

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from rest_framework.exceptions import NotFound, AuthenticationFailed
 from rest_framework import serializers
 from user.models import User, ProfileImage
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
     """회원가입 페이지, 회원 정보 수정 페이지에서 사용되는 시리얼라이저입니다."""
@@ -14,17 +17,21 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
 
-        profile_img = self.context['profile_img']
-        # print(profile_img)
-
-        for image_data in profile_img.getlist('profile_img'):
-            # print(image_data)
-            ProfileImage.objects.create(owner=user, profile_img=image_data)
-        return user
-
+        if self.context['profile_img']:
+            profile_img = self.context['profile_img']
+            # print(profile_img)
+            for image_data in profile_img.getlist('profile_img'):
+                # print(image_data)
+                ProfileImage.objects.create(owner=user, profile_img=image_data)
+            return user
+        else:
+            profile_img = settings.DEFAULT_PROFILE_IMAGE
+            ProfileImage.objects.create(owner=user, profile_img=profile_img)
+            return user
+        
 class LoginSerializer(TokenObtainPairSerializer):
     """DRF의 JWT 로그인 방식에 사용되는 TokenObtainPairSerializer를 상속하여 Serializer를 커스터마이징하여 재정의합니다."""
-
+    
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
