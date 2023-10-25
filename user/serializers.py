a25a5cef0fb5b1e2a7da40697fa1c7b7ebe63a85
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-10-25 22:53:55 +0900

    Add: 토큰에 사용자 intro 추가

--- user/serializers.py
+++ user/serializers.py
@@ -61,6 +61,7 @@ class LoginSerializer(TokenObtainPairSerializer):
         user_profile_img = ProfileImage.objects.get(owner=user)
         token['nickname'] = user.nickname
         token['profile_img'] = str(user_profile_img.profile_img)
+        token['intro'] = user.intro
         
         return {
             'access': str(token.access_token),
